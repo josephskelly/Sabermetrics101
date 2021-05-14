61bb6d2f67a06806a41d8464ac63a5403952c2e6
Joseph Kelly <jskelly84@gmail.com>
2021-05-14 12:11:39 -0700

    Adds shortstops from the year 2000.

--- main.R
+++ main.R
@@ -6,10 +6,37 @@
 # install.packages("ggplot2")
 # library(ggplot2)
 
+# use ?function to look up documentation for function
+
+### Data Frames
 batting <- read.csv("Sabr101x_PitchersB.csv")
 pitching <- read.csv("Sabr101x_PitchersP.csv")
+shortstops <- read.csv("2000_MLB_SS.csv")
+
 
 war <- batting$WAR
 max(war)
 min(war)
 sd(war)
+
+?subset
+fiftyPA <- subset(batting, PA >= 50)
+
+?ggplot
+
+# subset of players with OPS over .750
+
+slg750 <- subset(shortstops, subset = (OBP + SLG) > .750)
+min(slg750$Fld)
+worstFielder <- subset (slg750, subset = Fld == min(slg750$Fld))
+worstFielder$Name
+
+ip100 <- subset(pitching, IP > 100)
+pPlot <- ggplot(ip100, aes(K.9, ERA))
+pPlot <- pPlot + geom_point(color = "firebrick3")
+pPlot <- pPlot + ggtitle("ERA v K/9 for 2000-2009, min. 100 IP")
+pPlot <- pPlot + xlab("Strikeouts per 9")
+pPlot <- pPlot + ylab("Earned Run Average")
+pPlot <- pPlot + stat_smooth(method = "lm", formula = y ~ x, color = "black",
+                             size = 1)
+pPlot
